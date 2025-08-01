56374926c68c4e5bba641355ed6af23ebc53f5f7
Joel-P67 <jo.petrazoller@gmail.com>
2025-08-01 19:26:38 +0200

    mise a jour du bouton de telechargement

--- script/cv.js
+++ script/cv.js
@@ -1,11 +1,13 @@
 document.getElementById('download-pdf').addEventListener('click', () => {
-    const element = document.getElementById('cv'); // Ton bloc principal
+    const element = document.getElementById('cv');
+
     const opt = {
-        margin: 0.5,
+        margin: 0,
         filename: 'CV_Joel_Petrazoller.pdf',
         image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+        html2canvas: { scale: 2, backgroundColor: null },
+        jsPDF: { unit: 'pt', format: 'a4', orientation: 'portrait' }
     };
+
     html2pdf().set(opt).from(element).save();
 });
