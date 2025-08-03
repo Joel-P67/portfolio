16e948cd2c04aa1f5cb6670420d3823edb8063a2
Joel-P67 <jo.petrazoller@gmail.com>
2025-08-04 01:49:27 +0200

    corrections

--- script/header.js
+++ script/header.js
@@ -14,36 +14,4 @@ fetch('/portfolio/commun/header.html')
     burger.addEventListener('click', () => {
       menu.classList.toggle('active');
     });
-
-    // select thème - custom select
-    const button = document.getElementById('themeButton');
-    const options = document.getElementById('themeOptions');
-    const optionItems = options.querySelectorAll('.option');
-
-    button.addEventListener('click', () => {
-      options.style.display = options.style.display === 'block' ? 'none' : 'block';
-    });
-
-    optionItems.forEach(option => {
-      option.addEventListener('click', () => {
-        const theme = option.dataset.value;
-        document.documentElement.setAttribute('data-theme', theme);
-        localStorage.setItem('theme', theme);
-
-        const darkLink = document.querySelector('link[href*="dark.css"]');
-        const lightLink = document.querySelector('link[href*="light.css"]');
-
-        if (theme === 'dark') {
-          darkLink.disabled = false;
-          lightLink.disabled = true;
-          button.textContent = 'Thème : Sombre';
-        } else {
-          darkLink.disabled = true;
-          lightLink.disabled = false;
-          button.textContent = 'Thème : Clair';
-        }
-
-        options.style.display = 'none';
-      });
-    });
   });
